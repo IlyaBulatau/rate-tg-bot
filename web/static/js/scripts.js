d9e9e9f1c77fb222fd6caaa887cef9101ef746bf
IlyaBulatau <ilyabulatau@gmail.com>
2024-03-08 17:46:10 +0200

    added team select

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,9 +1,10 @@
 function showNextField() {
-  // Скрываем поле выбора страны и кнопку
   document.getElementById("country").style.display = "none";
   document.getElementById("countryLabel").style.display = "none";
   document.getElementById("ageGroup").style.display = "block";
   document.getElementById("ageGroupLabel").style.display = "block";
-  // Показываем кнопку "Отправить"
+  document.getElementById("participantType").style.display = "block";
+  document.getElementById("participantTypeLabel").style.display = "block";
   document.getElementById("sendData").style.display = "block";
+  document.getElementById("confirmButton").style.display = "none";
 }
