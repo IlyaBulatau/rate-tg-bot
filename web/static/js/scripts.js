564719d3b28ef595e82df703e5bbb4baebc949d4
IlyaBulatau <ilyabulatau@gmail.com>
2024-03-08 19:38:24 +0200

    mod html js files

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,24 +1,22 @@
 let rForm = document.getElementById("registrationForm");
 let countrySelect = document.getElementById("country");
 let ageGroupSelect = document.getElementById("ageGroup");
-let categoryTypeSelect = document.getElementById("categoryType");
 let participantTypeSelect = document.getElementById("participantType");
+
 
 document.getElementById("confirmCountry").addEventListener("click", function() {
     if (countrySelect.value !== "") {
-        showNextField("confirmCountry", "confirmCategory");
-    }
-});
-
-document.getElementById("confirmCategory").addEventListener("click", function() {
-    if (categoryTypeSelect.value !== "") {
-        showNextField("confirmCategory", "confirmAgeGroup");
+        showNextButton("confirmCountry", "confirmAgeGroup");
+        showNextLabel("countryLabel", "ageGroupLabel");
+        showNextSelect("country", "ageGroup");
     }
 });
 
 document.getElementById("confirmAgeGroup").addEventListener("click", function() {
     if (ageGroupSelect.value !== "") {
-        showNextField("confirmAgeGroup", "confirmParticipantType");
+        showNextButton("confirmAgeGroup", "confirmParticipantType");
+        showNextLabel("ageGroupLabel", "participantTypeLabel");
+        showNextSelect("ageGroup", "participantType");
     }
 });
 
@@ -29,10 +27,21 @@ document.getElementById("confirmParticipantType").addEventListener("click", func
 });
 
 
-function showNextField(currentButtonId, nextButtonId) {
+function showNextButton(currentButtonId, nextButtonId) {
     document.getElementById(currentButtonId).style.display = "none";
     document.getElementById(nextButtonId).style.display = "block";
 }
+
+function showNextLabel(currentLabelId, nextLabelId) {
+  document.getElementById(currentLabelId).style.display = "none";
+  document.getElementById(nextLabelId).style.display = "block";
+}
+
+function showNextSelect(currentSelectId, nextSelectId) {
+  document.getElementById(currentSelectId).style.display = "none";
+  document.getElementById(nextSelectId).style.display = "block";
+}
+
 
 function showSendButton() {
     document.getElementById("sendData").style.display = "block";
