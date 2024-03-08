a3dfe3f30dfd1f9330e6eed6fae93bb53987acca
IlyaBulatau <ilyabulatau@gmail.com>
2024-03-08 18:58:47 +0200

    mod front files

--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,10 +1,51 @@
-function showNextField() {
-  document.getElementById("country").style.display = "none";
-  document.getElementById("countryLabel").style.display = "none";
-  document.getElementById("ageGroup").style.display = "block";
-  document.getElementById("ageGroupLabel").style.display = "block";
-  document.getElementById("participantType").style.display = "block";
-  document.getElementById("participantTypeLabel").style.display = "block";
-  document.getElementById("sendData").style.display = "block";
-  document.getElementById("confirmButton").style.display = "none";
+let tg = window.Telegram.WebApp;
+let rForm = document.getElementById("registrationForm");
+let countrySelect = document.getElementById("country");
+let ageGroupSelect = document.getElementById("ageGroup");
+let categoryTypeSelect = document.getElementById("categoryType");
+let participantTypeSelect = document.getElementById("participantType");
+
+document.getElementById("confirmCountry").addEventListener("click", function() {
+    if (countrySelect.value !== "") {
+        showNextField("confirmCountry", "confirmCategory");
+    }
+});
+
+document.getElementById("confirmCategory").addEventListener("click", function() {
+    if (categoryTypeSelect.value !== "") {
+        showNextField("confirmCategory", "confirmAgeGroup");
+    }
+});
+
+document.getElementById("confirmAgeGroup").addEventListener("click", function() {
+    if (ageGroupSelect.value !== "") {
+        showNextField("confirmAgeGroup", "confirmParticipantType");
+    }
+});
+
+document.getElementById("confirmParticipantType").addEventListener("click", function() {
+    if (participantTypeSelect.value !== "") {
+        showSendButton();
+    }
+});
+
+document.getElementById("sendData").addEventListener("click", function() {
+    // Отправка данных в Telegram
+    let formData = {
+        "country": countrySelect.value,
+        "ageGroup": ageGroupSelect.value,
+        "categoryType": categoryTypeSelect.value,
+        "participantType": participantTypeSelect.value
+    };
+    tg.sendData(JSON.stringify(formData));
+    tg.close();
+});
+
+function showNextField(currentButtonId, nextButtonId) {
+    document.getElementById(currentButtonId).style.display = "none";
+    document.getElementById(nextButtonId).style.display = "block";
+}
+
+function showSendButton() {
+    document.getElementById("sendData").style.display = "block";
 }
