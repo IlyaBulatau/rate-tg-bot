e51e8ae3c03005d6a5747ade2ed586e7efef154f
IlyaBulatau <ilyabulatau@gmail.com>
2024-03-09 13:37:21 +0200

    added js

--- web/main.py
+++ web/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
@@ -52,6 +52,307 @@ CATEGORIES_COUNTRY = {
         ],
 }
 
+
+COMPETITIONS = {
+        "ru": {
+            Categories.PRESCHOOL.name: {
+                "team": {
+                    "tvorcheskaka": "Творческая категория", 
+                    "moveLine": "Движение по линии", 
+                    "sumo": "Сумо", 
+                    "futureInjener": "Миссия 'Будущий инженер'", 
+                    "scratch": "Scratch junior"
+                    }
+                ,
+                "individual": {
+                    "tvorcheskaka": "Творческая категория", 
+                    "moveLine": "Движение по линии", 
+                    "sumo": "Сумо", 
+                    "futureInjener": "Миссия 'Будущий инженер'", 
+                    "scratch": "Scratch junior"
+                    }
+            },
+            Categories.JUNIOR.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                },
+                "individual": {
+                    "startRobo": "Олимпиада «Старт в робототехнику»",
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "scratch": "Scratch",
+                }
+            },
+            Categories.MIDDLE.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                    "virulreal": "Виртуальная реальность",
+                    "bpla": "БПЛА",
+                    "internetCl": "Интернет вещей",
+                    "chpu": "Станки ЧПУ",
+                    "neirotech": "Нейротехнологии",
+                    "gameDev": "Разработка игр"
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                    "virulreal": "Виртуальная реальность",
+                    "bpla": "БПЛА",
+                    "chpu": "Станки ЧПУ",
+                    "neirotech": "Нейротехнологии",
+                    "python": "Python",
+                    "gameDev": "Разработка игр"
+                }
+            },
+            Categories.SENIOR.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                    "virulreal": "Виртуальная реальность",
+                    "bpla": "БПЛА",
+                    "internetCl": "Интернет вещей",
+                    "chpu": "Станки ЧПУ",
+                    "neirotech": "Нейротехнологии",
+                    "gameDev": "Разработка игр"
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                    "virulreal": "Виртуальная реальность",
+                    "bpla": "БПЛА",
+                    "chpu": "Станки ЧПУ",
+                    "neirotech": "Нейротехнологии",
+                    "python": "Python",
+                    "gameDev": "Разработка игр"
+                }
+            },
+            Categories.TEACHER.name: {},
+            Categories.OVZ.name: {},
+        },
+        "kz": {
+            Categories.JUNIOR.name: {
+                "team": {
+                   "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",  
+                },
+                "individual": {
+                    "startRobo": "Олимпиада «Старт в робототехнику»",
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "scratch": "Scratch",
+                }
+            },
+            Categories.MIDDLE.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "python": "Python",
+                    "scratch": "Scratch",
+                }
+            },
+            Categories.SENIOR.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "python": "Python",
+                    "scratch": "Scratch",
+                }
+            },
+        },
+        "uz": {
+            Categories.JUNIOR.name: {
+                "team": {
+                   "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",  
+                },
+                "individual": {
+                    "startRobo": "Олимпиада «Старт в робототехнику»",
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "waterRobot": "Подводные роботы",
+                    "modelDDD": "3Д моделирование",
+                    "scratch": "Scratch",
+                }
+            },
+            Categories.MIDDLE.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "python": "Python",
+                    "scratch": "Scratch",
+                }
+            },
+            Categories.SENIOR.name: {
+                "team": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "modelDDD": "3Д моделирование",
+                    "footboll": "Футбол",
+                },
+                "individual": {
+                    "moveLineDIY": "Движение по линии DIY",
+                    "moveLineConst": "Движение по линии Конструкторы",
+                    "sumoMech": "Механическое сумо",
+                    "sumoIntel": "Интеллектуальное сумо",
+                    "circl": "Эстафета",
+                    "trip": "Большое путешествие",
+                    "missTanos": "Миссия 'Вселенная бесконечна'",
+                    "tvorcheskaya": "Творческая категория",
+                    "python": "Python",
+                    "scratch": "Scratch",
+                }
+            },
+        },
+    }
+
+
 @app.get("/auth", response_class=HTMLResponse)
 async def form_authentication(request: Request):
     """Страница с формой"""
@@ -64,7 +365,7 @@ async def form_authentication(request: Request):
 
 @app.get("/auth/categories")
 async def get_categories(category: str):
-    return {"cateogries": CATEGORIES_COUNTRY[category]}
+    return JSONResponse(content={"categories": CATEGORIES_COUNTRY[category]})
 
 
 @app.post("/auth")
@@ -73,4 +374,9 @@ async def authentication(request: Request):
     print(request.__dict__)
     return RedirectResponse("/auth", status_code=302)
 
+
+@app.get("/auth/competitions")
+async def get_categories(country: str, category: str, practic: str):
+    response = COMPETITIONS[country][category][practic]
+    return JSONResponse(content=response)
 
--- web/static/js/scripts.js
+++ web/static/js/scripts.js
@@ -1,32 +1,64 @@
 let rForm = document.getElementById("registrationForm");
 let countrySelect = document.getElementById("country");
+let countryOtherSelect = document.getElementById("otherCounty")
 let ageGroupSelect = document.getElementById("ageGroup");
 let participantTypeSelect = document.getElementById("participantType");
+let competitionSelect = document.getElementById("competitionSelect");
 const rootURL = window.location.origin;
 
 document.getElementById("confirmCountry").addEventListener("click", function() {
   // кнопка выбор страны
-    if (countrySelect.value !== "") {
-        showNextButton("confirmCountry", "confirmAgeGroup");
-        showNextLabel("countryLabel", "ageGroupLabel");
-        showNextSelect("country", "ageGroup");
-        let country = countrySelect.value;
-        let url = new URL(rootURL+"/auth/categories");
-        url.searchParams.set("category", country);
-        const resp = new XMLHttpRequest();
-        resp.open("GET", url);
-        resp.responseType = "json";
-        resp.send();
-        var categories_data = JSON.parse(resp.responseText);
-        categories_data.array.forEach(element => {
-          for (var [key, val] of element) {
-            let option = document.createElement("option");
-            option.value = key;
-            option.text = val;
-            ageGroupSelect.add(option);
-          };
-        });
+    let country = countrySelect.value;
+    if (country !== "") {
+        if (country === "by") {
+          document.getElementById("confirmCountry").style.display = "none";
+          document.getElementById("countryLabel").style.display = "none";
+          document.getElementById("country").style.display = "none";
+
+          document.getElementById("byCountryLink").style.display = "block";
+        }
+
+        else if (country === "oth") {
+          document.getElementById("confirmCountry").style.display = "none";
+          document.getElementById("countryLabel").style.display = "none";
+          document.getElementById("country").style.display = "none";
+
+          document.getElementById("otherCountyLabel").style.display = "block";
+          document.getElementById("otherCounty").style.display = "block";
+          document.getElementById("otherCountyButton").style.display = "block";
+
+        }
+
+        else {
+          showNextButton("confirmCountry", "confirmAgeGroup");
+          showNextLabel("countryLabel", "ageGroupLabel");
+          showNextSelect("country", "ageGroup");
+  
+          let url = new URL(rootURL+"/auth/categories");
+          url.searchParams.set("category", country);
+  
+          fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            data["categories"].forEach(element => {
+              Object.entries(element).forEach(([key, val]) => {
+                let option = document.createElement("option");
+                option.value = key;
+                option.text = val;
+                ageGroupSelect.add(option);
+              });
+            });
+          })
+        }
+       
     }
+});
+
+document.getElementById("otherCountyButton").addEventListener("click", function() {
+    countrySelect.value = otherCounty.value
+    document.getElementById("sendData").style.display = "block";
 });
 
 document.getElementById("confirmAgeGroup").addEventListener("click", function() {
@@ -39,10 +71,41 @@ document.getElementById("confirmAgeGroup").addEventListener("click", function() 
 
 document.getElementById("confirmParticipantType").addEventListener("click", function() {
     if (participantTypeSelect.value !== "") {
-        showSendButton();
+        let country = countrySelect.value;
+        let age = ageGroupSelect.value;
+        let practic = participantTypeSelect.value;
+
+        let url = new URL(rootURL+"/auth/competitions");
+        url.searchParams.set("country", country);
+        url.searchParams.set("category", age);
+        url.searchParams.set("practic", practic);
+
+        fetch(url)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+              Object.entries(data).forEach(([key, val]) => {
+                let option = document.createElement("option");
+                option.value = key;
+                option.text = val;
+                competitionSelect.add(option);
+              });
+            });
+          
+
+        showNextButton("confirmParticipantType", "configrmCompetitionButton")
+        showNextLabel("participantTypeLabel", "competitionLabel")
+        showNextSelect("participantType", "competitionSelect")
     }
 });
 
+
+document.getElementById("configrmCompetitionButton").addEventListener("click", function() {
+  if (competitionSelect.value !== "") {
+      showSendButton();
+  }
+});
 
 function showNextButton(currentButtonId, nextButtonId) {
     document.getElementById(currentButtonId).style.display = "none";
